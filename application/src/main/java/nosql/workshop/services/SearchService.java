ea484300160fac778ade680ff6dac967200340f9
LfksBen <benjaminbraud@hotmail.fr>
2015-02-18 10:50:49 +0100

    Modification of services for default location

--- application/src/main/java/nosql/workshop/services/SearchService.java
+++ application/src/main/java/nosql/workshop/services/SearchService.java
@@ -29,83 +29,92 @@ import java.util.List;
  * Created by Chris on 12/02/15.
  */
 public class SearchService {
-    public static final String INSTALLATIONS_INDEX = "installations";
-    public static final String INSTALLATION_TYPE = "installation";
-    public static final String TOWNS_INDEX = "towns";
-    private static final String TOWN_TYPE = "town";
+	public static final String INSTALLATIONS_INDEX = "installations";
+	public static final String INSTALLATION_TYPE = "installation";
+	public static final String TOWNS_INDEX = "towns";
+	private static final String TOWN_TYPE = "town";
 
+	public static final String ES_HOST = "es.host";
+	public static final String ES_TRANSPORT_PORT = "es.transport.port";
 
-    public static final String ES_HOST = "es.host";
-    public static final String ES_TRANSPORT_PORT = "es.transport.port";
+	final Client elasticSearchClient;
+	final ObjectMapper objectMapper;
 
-    final Client elasticSearchClient;
-    final ObjectMapper objectMapper;
+	@Inject
+	public SearchService(@Named(ES_HOST) String host, @Named(ES_TRANSPORT_PORT) int transportPort) {
+		elasticSearchClient = new TransportClient(ImmutableSettings.settingsBuilder().put("cluster.name", "TheBestTocard").build())
+				.addTransportAddress(new InetSocketTransportAddress(host, transportPort));
 
-    @Inject
-    public SearchService(@Named(ES_HOST) String host, @Named(ES_TRANSPORT_PORT) int transportPort) {
-        elasticSearchClient = new TransportClient(ImmutableSettings.settingsBuilder().put("cluster.name", "TheBestTocard")
-				.build()).addTransportAddress(new InetSocketTransportAddress(host, transportPort));
+		objectMapper = new ObjectMapper();
+		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+	}
 
-        objectMapper = new ObjectMapper();
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-    }
+	/**
+	 * Recherche les installations à l'aide d'une requête full-text
+	 * 
+	 * @param searchQuery
+	 *            la requête
+	 * @return la listes de installations
+	 */
+	public List<Installation> search(String searchQuery) {
+		// TODO codez le service
+		throw new UnsupportedOperationException();
+	}
 
-    /**
-     * Recherche les installations à l'aide d'une requête full-text
-     * @param searchQuery la requête
-     * @return la listes de installations
-     */
-    public List<Installation> search(String searchQuery) {
-        // TODO codez le service
-        throw new UnsupportedOperationException();
-    }
-
-    /**
-     * Transforme un résultat de recherche ES en objet installation.
-     *
-     * @param searchHit l'objet ES.
-     * @return l'installation.
-     */
-    private Installation mapToInstallation(SearchHit searchHit) {
-        try {
-            return objectMapper.readValue(searchHit.getSourceAsString(), Installation.class);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public List<TownSuggest> suggestTownName(String townName){
-    	List<TownSuggest> listTownSuggest = new ArrayList<TownSuggest>();
-    	SearchResponse response = elasticSearchClient.prepareSearch("towns")
-    		.setQuery(QueryBuilders.queryString("townName:" + townName + "*"))
-    		.execute().actionGet();
-    	Iterator<SearchHit> iteratorHit = response.getHits().iterator();
+	/**
+	 * Transforme un résultat de recherche ES en objet installation.
+	 *
+	 * @param searchHit
+	 *            l'objet ES.
+	 * @return l'installation.
+	 */
+	private Installation mapToInstallation(SearchHit searchHit) {
 		try {
-	    	while(iteratorHit.hasNext()){
-	    		SearchHit searchHit = iteratorHit.next();
-				listTownSuggest.add(objectMapper.readValue(searchHit.getSourceAsString(), TownSuggest.class));
-	    	}
+			return objectMapper.readValue(searchHit.getSourceAsString(), Installation.class);
 		} catch (IOException e) {
-			 throw new RuntimeException(e);
+			throw new RuntimeException(e);
 		}
-    	return listTownSuggest;
-    }
+	}
 
-    public Double[] getTownLocation(String townName) {
-    	SearchResponse response = elasticSearchClient.prepareSearch("towns")
-    								.setTypes("town")
-    								.setQuery("townName:"+townName)
-    								.execute()
-    								.actionGet();
-    	
-    	SearchHits searchHits = response.getHits();
-    	TownSuggest townSuggest = null;
-    	try {
-			townSuggest = objectMapper.readValue(searchHits.getHits()[0].getSourceAsString(), TownSuggest.class);
-    	 } catch (IOException e) {
-             throw new RuntimeException(e);
-         }
-    	
-       return townSuggest.getLocation();
-    }
+	/**
+	 * Transforme un résultat de recherche ES en objet installation.
+	 *
+	 * @param searchHit
+	 *            l'objet ES.
+	 * @return l'installation.
+	 */
+	private TownSuggest mapToTownSuggest(SearchHit searchHit) {
+		try {
+			return objectMapper.readValue(searchHit.getSourceAsString(), TownSuggest.class);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public List<TownSuggest> suggestTownName(String townName) {
+		List<TownSuggest> listTownSuggest = new ArrayList<TownSuggest>();
+		SearchResponse response = elasticSearchClient.prepareSearch("towns")
+				.setQuery(QueryBuilders.queryString("townName:" + townName + "*")).execute().actionGet();
+		Iterator<SearchHit> iteratorHit = response.getHits().iterator();
+		while (iteratorHit.hasNext()) {
+			SearchHit searchHit = iteratorHit.next();
+			listTownSuggest.add(mapToTownSuggest(searchHit));
+		}
+		return listTownSuggest;
+	}
+
+	public Double[] getTownLocation(String townName) {
+		SearchResponse response = elasticSearchClient.prepareSearch("towns").setTypes("town")
+				.setQuery(QueryBuilders.queryString("townName:" + townName)).execute()
+				.actionGet();
+
+		SearchHits searchHits = response.getHits();
+		// by default we set the location to Carquefou
+		Double[] location = new Double[] { -1.49295, 47.29692 };
+		if (searchHits.getHits().length > 0) {
+			location = mapToTownSuggest(searchHits.getHits()[0]).getLocation();
+		}
+
+		return location;
+	}
 }
