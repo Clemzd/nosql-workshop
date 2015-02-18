23b4b659c5de3fe8d81af6aebd729d00b557b0b5
Clement <clement.guet@gmail.com>
2015-02-18 09:06:51 +0100

    import towns

--- batch/src/main/java/nosql/workshop/batch/elasticsearch/ImportTowns.java
+++ batch/src/main/java/nosql/workshop/batch/elasticsearch/ImportTowns.java
@@ -1,53 +1,62 @@
 package nosql.workshop.batch.elasticsearch;
 
-import nosql.workshop.batch.elasticsearch.util.ElasticSearchBatchUtils;
-import org.elasticsearch.action.bulk.BulkRequestBuilder;
-import org.elasticsearch.action.bulk.BulkResponse;
-import org.elasticsearch.client.Client;
-import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import static nosql.workshop.batch.elasticsearch.util.ElasticSearchBatchUtils.dealWithFailures;
+import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-import static nosql.workshop.batch.elasticsearch.util.ElasticSearchBatchUtils.checkIndexExists;
-import static nosql.workshop.batch.elasticsearch.util.ElasticSearchBatchUtils.dealWithFailures;
+import nosql.workshop.batch.elasticsearch.util.ElasticSearchBatchUtils;
+
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.ImmutableSettings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
 
 /**
- * Job d'import des rues de towns_paysdeloire.csv vers ElasticSearch (/towns/town)
+ * Job d'import des rues de towns_paysdeloire.csv vers ElasticSearch
+ * (/towns/town)
  */
 public class ImportTowns {
-    public static void main(String[] args) throws IOException {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(ImportTowns.class.getResourceAsStream("/csv/towns_paysdeloire.csv")));
-             Client elasticSearchClient = new TransportClient().addTransportAddress(new InetSocketTransportAddress("localhost", 9300));) {
+	public static void main(String[] args) throws IOException {
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(
+				ImportTowns.class.getResourceAsStream("/csv/towns_paysdeloire.csv")));
+				Client elasticSearchClient = new TransportClient(ImmutableSettings.settingsBuilder().put("cluster.name", "TheBestTocard")
+						.build()).addTransportAddress(new InetSocketTransportAddress("localhost", 9300));) {
 
-            checkIndexExists("towns", elasticSearchClient);
+			// checkIndexExists("towns", elasticSearchClient);
 
-            BulkRequestBuilder bulkRequest = elasticSearchClient.prepareBulk();
+			BulkRequestBuilder bulkRequest = elasticSearchClient.prepareBulk();
 
-            reader.lines()
-                    .skip(1)
-                    .filter(line -> line.length() > 0)
-                    .forEach(line -> insertTown(line, bulkRequest, elasticSearchClient));
+			reader.lines().skip(1).filter(line -> line.length() > 0).forEach(line -> insertTown(line, bulkRequest, elasticSearchClient));
 
-            BulkResponse bulkItemResponses = bulkRequest.execute().actionGet();
+			BulkResponse bulkItemResponses = bulkRequest.execute().actionGet();
 
-            dealWithFailures(bulkItemResponses);
-        }
+			dealWithFailures(bulkItemResponses);
+		}
 
-    }
+	}
 
-    private static void insertTown(String line, BulkRequestBuilder bulkRequest, Client elasticSearchClient) {
-        line = ElasticSearchBatchUtils.handleComma(line);
+	private static void insertTown(String line, BulkRequestBuilder bulkRequest, Client elasticSearchClient) {
+		line = ElasticSearchBatchUtils.handleComma(line);
 
-        String[] split = line.split(",");
+		String[] split = line.split(",");
 
-        String townName = split[1].replaceAll("\"", "");
-        Double longitude = Double.valueOf(split[6]);
-        Double latitude = Double.valueOf(split[7]);
+		String townName = split[1].replaceAll("\"", "");
+		Double longitude = Double.valueOf(split[6]);
+		Double latitude = Double.valueOf(split[7]);
 
-        // TODO ajoutez le code permettant d'insérer la ville
-        
-    }
+		// TODO ajoutez le code permettant d'insérer la ville
+		try {
+			bulkRequest.add(elasticSearchClient.prepareIndex("towns", "town").setSource(
+					jsonBuilder().startObject().field("townName", townName).field("longitude", longitude).field("latitude", latitude)
+							.endObject()));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
